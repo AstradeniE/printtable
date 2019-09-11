5aeb2f7eee488bafad845a1bde0c395b672de158
AstradeniE <astradeniefth@gmail.com>
2019-09-11 16:11:20 +0300

    power function

--- printtable.py
+++ printtable.py
@@ -1,5 +1,7 @@
-def multtable(number, limit):
+def powertable(number, limit):
     for i in range(1, limit+1):
-        print(i * number)
+        print(i ** number)
 
-multtable(7, 10)
+powertable(2, 4)
+powertable(3, 3)
+
